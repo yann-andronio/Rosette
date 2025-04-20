2cbad44c70b294c80ef3f1b8f0ffc68eb118d4be
AZCOM <145325495+yann-andronio@users.noreply.github.com>
2025-04-20 14:20:16 +0300

    correction de bug usefilterdata 2.0

--- src/renderer/src/hooks/useFilterData.tsx
+++ src/renderer/src/hooks/useFilterData.tsx
@@ -3,11 +3,8 @@ import { filterDatasearch } from '../utils/Ufilterdatasearch'
 
 export function useFilterData<T>(data: T[], search: string, keys: (keyof T)[]): T[] {
   const [filtered, setFiltered] = useState<T[]>(data)
-
-  // Utilisation de useRef pour garder une référence à la version initiale de `data`
   const dataRef = useRef<T[]>(data)
 
-  // Mettre à jour `dataRef` seulement quand `data` change
   useEffect(() => {
     dataRef.current = data
   }, [data])
@@ -15,7 +12,6 @@ export function useFilterData<T>(data: T[], search: string, keys: (keyof T)[]): 
   useEffect(() => {
     const result = filterDatasearch(dataRef.current, search, keys)
 
-    // Ne mettre à jour `filtered` que si le résultat change
     if (JSON.stringify(result) !== JSON.stringify(filtered)) {
       setFiltered(search === '' ? [...dataRef.current] : result)
     }
