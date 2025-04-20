5a14c3a60b1e4fbd1823aa6b6da744d93061313e
AZCOM <145325495+yann-andronio@users.noreply.github.com>
2025-04-20 20:53:02 +0300

    creation de formulaire chaud

--- src/renderer/src/components/modalsform/AdUpinfostudents.tsx
+++ src/renderer/src/components/modalsform/AdUpinfostudents.tsx
@@ -10,7 +10,7 @@ type SearchBarProps = {
 
 const AdUpinfostudents: React.FC<SearchBarProps> = ({ closemodal }) => {
   const ValidationSchema = yup.object({
-      classadd: yup.string().required('vous devez saisir une nom de classe'),
+    classadd: yup.string().required('vous devez saisir une nom de classe'),
     nom: yup.string().required('Nom requis'),
     prenom: yup.string().required('Prénom requis'),
     adresse: yup.string().required('Adresse requise'),
@@ -60,8 +60,7 @@ const AdUpinfostudents: React.FC<SearchBarProps> = ({ closemodal }) => {
             <FiX size={20} />
           </button>
         </div>
-        <div className="bigboxform flex gap-4">
-
+        <div className="bigboxform flex gap-10">
           {/* section111 */}
           <div className="flex-1 ">
             <div className="champ1 flex flex-col gap-4 ">
@@ -127,10 +126,228 @@ const AdUpinfostudents: React.FC<SearchBarProps> = ({ closemodal }) => {
               </div>
             </div>
           </div>
-
           {/* section2222 */}
-          <div className="flex-1"></div>
-          <div className="flex-1"></div>
+          <div className="flex-1">
+            <div className="champ2 flex flex-col gap-4 ">
+              <div className="adresse">
+                <label className="block font-medium text-gray-700 mb-1">date de naissance</label>
+                <input
+                  type="date"
+                  {...register('date_naissance')}
+                  className={`w-full px-4 py-2.5 border border-[#895256] ${
+                    errors.date_naissance ? 'border-red-400' : 'border-gray-300'
+                  } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
+                />
+                {errors.date_naissance && (
+                  <p className="text-sm text-red-400 mt-1">{errors.date_naissance.message}</p>
+                )}
+              </div>
+
+              <div className="adresse">
+                <label className="block font-medium text-gray-700 mb-1">lieu de naissance</label>
+                <input
+                  type="text"
+                  placeholder="Tana"
+                  {...register('lieu_naissance')}
+                  className={`w-full px-4 py-2.5 border border-[#895256] ${
+                    errors.lieu_naissance ? 'border-red-400' : 'border-gray-300'
+                  } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
+                />
+                {errors.lieu_naissance && (
+                  <p className="text-sm text-red-400 mt-1">{errors.lieu_naissance.message}</p>
+                )}
+              </div>
+
+              <div className="nom_pereandprenom_pere flex gap-3">
+                <div className="nom_pere">
+                  <label className="block font-medium text-gray-700 mb-1">nom du pere</label>
+                  <input
+                    type="text"
+                    placeholder="rajao"
+                    {...register('nom_pere')}
+                    className={`w-full px-4 py-2.5 border border-[#895256] ${
+                      errors.nom_pere ? 'border-red-400' : 'border-gray-300'
+                    } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
+                  />
+                  {errors.nom_pere && (
+                    <p className="text-sm text-red-400 mt-1">{errors.nom_pere.message}</p>
+                  )}
+                </div>
+
+                <div className="prenom">
+                  <label className="block font-medium text-gray-700 mb-1">prenom du pere</label>
+                  <input
+                    type="text"
+                    placeholder="kely"
+                    {...register('prenom_pere')}
+                    className={`w-full px-4 py-2.5 border border-[#895256] ${
+                      errors.prenom_pere ? 'border-red-400' : 'border-gray-300'
+                    } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
+                  />
+                  {errors.prenom_pere && (
+                    <p className="text-sm text-red-400 mt-1">{errors.prenom_pere.message}</p>
+                  )}
+                </div>
+              </div>
+
+              <div className="nom_mereandprenom_pere flex gap-3">
+                <div className="nom_mere">
+                  <label className="block font-medium text-gray-700 mb-1">nom du mere</label>
+                  <input
+                    type="text"
+                    placeholder="rasoa"
+                    {...register('nom_mere')}
+                    className={`w-full px-4 py-2.5 border border-[#895256] ${
+                      errors.nom_mere ? 'border-red-400' : 'border-gray-300'
+                    } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
+                  />
+                  {errors.nom_mere && (
+                    <p className="text-sm text-red-400 mt-1">{errors.nom_mere.message}</p>
+                  )}
+                </div>
+
+                <div className="prenom">
+                  <label className="block font-medium text-gray-700 mb-1">prenom du mere</label>
+                  <input
+                    type="text"
+                    placeholder="be"
+                    {...register('prenom_mere')}
+                    className={`w-full px-4 py-2.5 border border-[#895256] ${
+                      errors.prenom_mere ? 'border-red-400' : 'border-gray-300'
+                    } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
+                  />
+                  {errors.prenom_mere && (
+                    <p className="text-sm text-red-400 mt-1">{errors.prenom_mere.message}</p>
+                  )}
+                </div>
+              </div>
+
+              <div className="tel_pere">
+                <label className="block font-medium text-gray-700 mb-1">telephone du pere</label>
+                <input
+                  type="tel"
+                  {...register('tel_pere')}
+                  className={`w-full px-4 py-2.5 border border-[#895256] ${
+                    errors.tel_pere ? 'border-red-400' : 'border-gray-300'
+                  } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
+                />
+                {errors.tel_pere && (
+                  <p className="text-sm text-red-400 mt-1">{errors.tel_pere.message}</p>
+                )}
+              </div>
+            </div>
+          </div>
+          {/* section33333 */}
+          <div className="flex-1">
+            <div className="champ2 flex flex-col gap-4 ">
+              <div className="adresse">
+                <label className="block font-medium text-gray-700 mb-1">date de naissance</label>
+                <input
+                  type="date"
+                  {...register('date_naissance')}
+                  className={`w-full px-4 py-2.5 border border-[#895256] ${
+                    errors.date_naissance ? 'border-red-400' : 'border-gray-300'
+                  } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
+                />
+                {errors.date_naissance && (
+                  <p className="text-sm text-red-400 mt-1">{errors.date_naissance.message}</p>
+                )}
+              </div>
+
+              <div className="adresse">
+                <label className="block font-medium text-gray-700 mb-1">lieu de naissance</label>
+                <input
+                  type="text"
+                  placeholder="Tana"
+                  {...register('lieu_naissance')}
+                  className={`w-full px-4 py-2.5 border border-[#895256] ${
+                    errors.lieu_naissance ? 'border-red-400' : 'border-gray-300'
+                  } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
+                />
+                {errors.lieu_naissance && (
+                  <p className="text-sm text-red-400 mt-1">{errors.lieu_naissance.message}</p>
+                )}
+              </div>
+
+              <div className="nom_pereandprenom_pere flex gap-3">
+                <div className="nom_pere">
+                  <label className="block font-medium text-gray-700 mb-1">nom du pere</label>
+                  <input
+                    type="text"
+                    placeholder="rajao"
+                    {...register('nom_pere')}
+                    className={`w-full px-4 py-2.5 border border-[#895256] ${
+                      errors.nom_pere ? 'border-red-400' : 'border-gray-300'
+                    } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
+                  />
+                  {errors.nom_pere && (
+                    <p className="text-sm text-red-400 mt-1">{errors.nom_pere.message}</p>
+                  )}
+                </div>
+
+                <div className="prenom">
+                  <label className="block font-medium text-gray-700 mb-1">prenom du pere</label>
+                  <input
+                    type="text"
+                    placeholder="kely"
+                    {...register('prenom_pere')}
+                    className={`w-full px-4 py-2.5 border border-[#895256] ${
+                      errors.prenom_pere ? 'border-red-400' : 'border-gray-300'
+                    } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
+                  />
+                  {errors.prenom_pere && (
+                    <p className="text-sm text-red-400 mt-1">{errors.prenom_pere.message}</p>
+                  )}
+                </div>
+              </div>
+
+              <div className="nom_mereandprenom_pere flex gap-3">
+                <div className="nom_mere">
+                  <label className="block font-medium text-gray-700 mb-1">nom du mere</label>
+                  <input
+                    type="text"
+                    placeholder="rasoa"
+                    {...register('nom_mere')}
+                    className={`w-full px-4 py-2.5 border border-[#895256] ${
+                      errors.nom_mere ? 'border-red-400' : 'border-gray-300'
+                    } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
+                  />
+                  {errors.nom_mere && (
+                    <p className="text-sm text-red-400 mt-1">{errors.nom_mere.message}</p>
+                  )}
+                </div>
+
+                <div className="prenom">
+                  <label className="block font-medium text-gray-700 mb-1">prenom du mere</label>
+                  <input
+                    type="text"
+                    placeholder="be"
+                    {...register('prenom_mere')}
+                    className={`w-full px-4 py-2.5 border border-[#895256] ${
+                      errors.prenom_mere ? 'border-red-400' : 'border-gray-300'
+                    } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
+                  />
+                  {errors.prenom_mere && (
+                    <p className="text-sm text-red-400 mt-1">{errors.prenom_mere.message}</p>
+                  )}
+                </div>
+              </div>
+
+              <div className="tel_pere">
+                <label className="block font-medium text-gray-700 mb-1">telephone du pere</label>
+                <input
+                  type="tel"
+                  {...register('tel_pere')}
+                  className={`w-full px-4 py-2.5 border border-[#895256] ${
+                    errors.tel_pere ? 'border-red-400' : 'border-gray-300'
+                  } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
+                />
+                {errors.tel_pere && (
+                  <p className="text-sm text-red-400 mt-1">{errors.tel_pere.message}</p>
+                )}
+              </div>
+            </div>
+          </div>{' '}
         </div>
 
         <div className="flex justify-end gap-3 mt-6">
