55429b768d14458dd9bdc37d8ae47a5744235470
AZCOM <145325495+yann-andronio@users.noreply.github.com>
2025-04-21 21:21:51 +0300

    trimestre3 ajouter

--- src/renderer/src/components/modalsform/Addnotemodal.tsx
+++ src/renderer/src/components/modalsform/Addnotemodal.tsx
@@ -15,7 +15,7 @@ const Addnotemodal: React.FC<SearchBarProps> = ({ closemodal }) => {
     classe: yup.string().required('classe requise'),
     trimestre1: yup.string().required('trimestre1 requis'),
     trimestre2: yup.string().required('Date de naissance requise'),
-    lieu_naissance: yup.string().required('Lieu de naissance requis'),
+    trimestre3: yup.string().required('Lieu de naissance requis'),
     nom_pere: yup.string(),
     prenom_pere: yup.string(),
     nom_mere: yup.string(),
@@ -149,13 +149,13 @@ const Addnotemodal: React.FC<SearchBarProps> = ({ closemodal }) => {
                 <input
                   type="text"
                   placeholder="Tana"
-                  {...register('lieu_naissance')}
+                  {...register('trimestre3')}
                   className={`w-full px-4 py-2.5 border border-[#895256]  bg-[#F1F1F1]  ${
-                    errors.lieu_naissance ? 'border-red-400' : 'border-gray-300'
+                    errors.trimestre3 ? 'border-red-400' : 'border-gray-300'
                   } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
                 />
-                {errors.lieu_naissance && (
-                  <p className="text-sm text-red-400 mt-1">{errors.lieu_naissance.message}</p>
+                {errors.trimestre3 && (
+                  <p className="text-sm text-red-400 mt-1">{errors.trimestre3.message}</p>
                 )}
               </div>
 
