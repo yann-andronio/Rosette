a701b91cad55b01f59230af971f88f8208f288c3
AZCOM <145325495+yann-andronio@users.noreply.github.com>
2025-04-21 21:29:33 +0300

    reorganisation 2.0

--- src/renderer/src/components/modalsform/Addnotemodal.tsx
+++ src/renderer/src/components/modalsform/Addnotemodal.tsx
@@ -92,22 +92,19 @@ const Addnotemodal: React.FC<SearchBarProps> = ({ closemodal }) => {
                   <p className="text-sm text-red-400 mt-1">{errors.trimestre3.message}</p>
                 )}
               </div>
-
-              <div className="nom_pereandtotalcoefficient flex gap-3">
-                <div className="prenom">
-                  <label className="block font-medium text-gray-700 mb-1">prenom du pere</label>
-                  <input
-                    type="text"
-                    placeholder="kely"
-                    {...register('totalcoefficient')}
-                    className={`w-full px-4 py-2.5 border border-[#895256]  bg-[#F1F1F1]  ${
-                      errors.totalcoefficient ? 'border-red-400' : 'border-gray-300'
-                    } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
-                  />
-                  {errors.totalcoefficient && (
-                    <p className="text-sm text-red-400 mt-1">{errors.totalcoefficient.message}</p>
-                  )}
-                </div>
+              <div className="totalcoefficient">
+                <label className="block font-medium text-gray-700 mb-1">total coefficient</label>
+                <input
+                  type="text"
+                  placeholder="Tana"
+                  {...register('trimestre3')}
+                  className={`w-full px-4 py-2.5 border border-[#895256]  bg-[#F1F1F1]  ${
+                    errors.totalcoefficient ? 'border-red-400' : 'border-gray-300'
+                  } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
+                />
+                {errors.totalcoefficient && (
+                  <p className="text-sm text-red-400 mt-1">{errors.totalcoefficient.message}</p>
+                )}
               </div>
 
               <div className="moyenneandtotalcoefficient flex gap-3">
