f63617a6fc76796f2c7e5472ac0461b5381e6f47
AZCOM <145325495+yann-andronio@users.noreply.github.com>
2025-04-22 21:16:54 +0300

    ass something note

--- src/renderer/src/components/modalsform/Addnotemodal.tsx
+++ src/renderer/src/components/modalsform/Addnotemodal.tsx
@@ -76,7 +76,7 @@ const Addnotemodal: React.FC<SearchBarProps> = ({ closemodal }) => {
                 />
                 {errors.nom && <p className="text-sm text-red-400 mt-1">{errors.nom.message}</p>}
               </div>
-              
+
               <div className="classe">
                 <label className="block font-medium text-gray-700 mb-1">prenom</label>
                 <input
@@ -121,6 +121,55 @@ const Addnotemodal: React.FC<SearchBarProps> = ({ closemodal }) => {
                   <p className="text-sm text-red-400 mt-1">{errors.totalcoefficient.message}</p>
                 )}
               </div>
+            </div>
+          </div>
+          {/* section33333333333333333333333333333333333333 */}
+          <div className="flex-1">
+            <div className="champ3 flex flex-col gap-4 ">
+              <div className="totalcoefficient">
+                <label className="block font-medium text-gray-700 mb-1">total coefficient</label>
+                <input
+                  type="text"
+                  placeholder="Tana"
+                  {...register('trimestre3')}
+                  className={`w-full px-4 py-2.5 border border-[#895256]  bg-[#F1F1F1]  ${
+                    errors.totalcoefficient ? 'border-red-400' : 'border-gray-300'
+                  } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
+                />
+                {errors.totalcoefficient && (
+                  <p className="text-sm text-red-400 mt-1">{errors.totalcoefficient.message}</p>
+                )}
+              </div>
+
+              <div className="totalcoefficient">
+                <label className="block font-medium text-gray-700 mb-1">total coefficient</label>
+                <input
+                  type="text"
+                  placeholder="Tana"
+                  {...register('trimestre3')}
+                  className={`w-full px-4 py-2.5 border border-[#895256]  bg-[#F1F1F1]  ${
+                    errors.totalcoefficient ? 'border-red-400' : 'border-gray-300'
+                  } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
+                />
+                {errors.totalcoefficient && (
+                  <p className="text-sm text-red-400 mt-1">{errors.totalcoefficient.message}</p>
+                )}
+              </div>
+
+              <div className="classe">
+                <label className="block font-medium text-gray-700 mb-1">classe</label>
+                <input
+                  type="text"
+                  placeholder="ambohipo"
+                  {...register('classe')}
+                  className={`w-full px-4 py-2.5 border border-[#895256]  bg-[#F1F1F1]  ${
+                    errors.classe ? 'border-red-400' : 'border-gray-300'
+                  } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
+                />
+                {errors.classe && (
+                  <p className="text-sm text-red-400 mt-1">{errors.classe.message}</p>
+                )}
+              </div>
 
               <div className="moyenneandtotalcoefficient flex gap-3">
                 <div className="moyenne">
@@ -155,55 +204,6 @@ const Addnotemodal: React.FC<SearchBarProps> = ({ closemodal }) => {
               </div>
             </div>
           </div>
-          {/* section33333333333333333333333333333333333333 */}
-          <div className="flex-1">
-            <div className="champ3 flex flex-col gap-4 ">
-              <div className="nomandprenom flex gap-3">
-                <div className="nom">
-                  <label className="block font-medium text-gray-700 mb-1">Nom</label>
-                  <input
-                    type="text"
-                    placeholder="rakoto"
-                    {...register('nom')}
-                    className={`w-full px-4 py-2.5 border border-[#895256]  bg-[#F1F1F1]  ${
-                      errors.nom ? 'border-red-400' : 'border-gray-300'
-                    } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
-                  />
-                  {errors.nom && <p className="text-sm text-red-400 mt-1">{errors.nom.message}</p>}
-                </div>
-
-                <div className="prenom">
-                  <label className="block font-medium text-gray-700 mb-1">prenom</label>
-                  <input
-                    type="text"
-                    placeholder="kely"
-                    {...register('prenom')}
-                    className={`w-full px-4 py-2.5 border border-[#895256]  bg-[#F1F1F1]  ${
-                      errors.prenom ? 'border-red-400' : 'border-gray-300'
-                    } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
-                  />
-                  {errors.prenom && (
-                    <p className="text-sm text-red-400 mt-1">{errors.prenom.message}</p>
-                  )}
-                </div>
-              </div>
-
-              <div className="classe">
-                <label className="block font-medium text-gray-700 mb-1">classe</label>
-                <input
-                  type="text"
-                  placeholder="ambohipo"
-                  {...register('classe')}
-                  className={`w-full px-4 py-2.5 border border-[#895256]  bg-[#F1F1F1]  ${
-                    errors.classe ? 'border-red-400' : 'border-gray-300'
-                  } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
-                />
-                {errors.classe && (
-                  <p className="text-sm text-red-400 mt-1">{errors.classe.message}</p>
-                )}
-              </div>
-            </div>
-          </div>{' '}
         </div>
 
         <div className="flex justify-end gap-3 mt-6">
