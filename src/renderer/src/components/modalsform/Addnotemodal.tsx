850cedb23265216908805c900d536083030444d6
AZCOM <145325495+yann-andronio@users.noreply.github.com>
2025-04-22 21:14:37 +0300

    petit modif

--- src/renderer/src/components/modalsform/Addnotemodal.tsx
+++ src/renderer/src/components/modalsform/Addnotemodal.tsx
@@ -68,13 +68,28 @@ const Addnotemodal: React.FC<SearchBarProps> = ({ closemodal }) => {
                 <label className="block font-medium text-gray-700 mb-1">Nom</label>
                 <input
                   type="text"
-                  placeholder="Tana"
+                  placeholder="rakoto"
                   {...register('nom')}
                   className={`w-full px-4 py-2.5 border border-[#895256]  bg-[#F1F1F1]  ${
                     errors.nom ? 'border-red-400' : 'border-gray-300'
                   } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
                 />
                 {errors.nom && <p className="text-sm text-red-400 mt-1">{errors.nom.message}</p>}
+              </div>
+              
+              <div className="classe">
+                <label className="block font-medium text-gray-700 mb-1">prenom</label>
+                <input
+                  type="text"
+                  placeholder="kely"
+                  {...register('prenom')}
+                  className={`w-full px-4 py-2.5 border border-[#895256]  bg-[#F1F1F1]  ${
+                    errors.prenom ? 'border-red-400' : 'border-gray-300'
+                  } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
+                />
+                {errors.prenom && (
+                  <p className="text-sm text-red-400 mt-1">{errors.prenom.message}</p>
+                )}
               </div>
 
               <div className="trimestre1">
@@ -92,20 +107,6 @@ const Addnotemodal: React.FC<SearchBarProps> = ({ closemodal }) => {
                 )}
               </div>
 
-              <div className="classe">
-                <label className="block font-medium text-gray-700 mb-1">prenom</label>
-                <input
-                  type="text"
-                  placeholder="Tana"
-                  {...register('prenom')}
-                  className={`w-full px-4 py-2.5 border border-[#895256]  bg-[#F1F1F1]  ${
-                    errors.prenom ? 'border-red-400' : 'border-gray-300'
-                  } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
-                />
-                {errors.prenom && (
-                  <p className="text-sm text-red-400 mt-1">{errors.prenom.message}</p>
-                )}
-              </div>
               <div className="totalcoefficient">
                 <label className="block font-medium text-gray-700 mb-1">total coefficient</label>
                 <input
