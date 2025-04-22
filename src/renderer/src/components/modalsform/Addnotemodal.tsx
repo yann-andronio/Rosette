11f6346ac2132b9d7e133b50e9c283bca33de094
AZCOM <145325495+yann-andronio@users.noreply.github.com>
2025-04-22 21:14:05 +0300

    update note1.12 ok

--- src/renderer/src/components/modalsform/Addnotemodal.tsx
+++ src/renderer/src/components/modalsform/Addnotemodal.tsx
@@ -93,17 +93,17 @@ const Addnotemodal: React.FC<SearchBarProps> = ({ closemodal }) => {
               </div>
 
               <div className="classe">
-                <label className="block font-medium text-gray-700 mb-1">lieu de naissance</label>
+                <label className="block font-medium text-gray-700 mb-1">prenom</label>
                 <input
                   type="text"
                   placeholder="Tana"
-                  {...register('trimestre3')}
+                  {...register('prenom')}
                   className={`w-full px-4 py-2.5 border border-[#895256]  bg-[#F1F1F1]  ${
-                    errors.trimestre3 ? 'border-red-400' : 'border-gray-300'
+                    errors.prenom ? 'border-red-400' : 'border-gray-300'
                   } rounded-lg focus:outline-none focus:ring-2 focus:ring-[#895256] text-gray-700 placeholder:text-gray-400`}
                 />
-                {errors.trimestre3 && (
-                  <p className="text-sm text-red-400 mt-1">{errors.trimestre3.message}</p>
+                {errors.prenom && (
+                  <p className="text-sm text-red-400 mt-1">{errors.prenom.message}</p>
                 )}
               </div>
               <div className="totalcoefficient">
